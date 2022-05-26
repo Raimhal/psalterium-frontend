8beb4e713349342c956d8db0319a08797f54f053
Raimhal <kormish2003@gmail.com>
2022-05-26 12:46:26 +0300

    .

--- src/router/instance.js
+++ src/router/instance.js
@@ -3,3 +3,12 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: `${process.env.VUE_APP_API_URL || "https://psalterium-backend.herokuapp.com"}/api`,
 });
+
+instance.interceptors.request.use((config) => {
+    if((JSON.parse(localStorage.getItem('isAuth')))) {
+        const token = `Bearer ${localStorage.getItem('accessToken')}`;
+        if (config && config.headers)
+            config.headers.Authorization = token ? token : '';
+    }
+    return config;
+});
